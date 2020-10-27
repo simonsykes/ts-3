f23f17a55a0c9372b2eadfe5b3ce615a9f2166a0
simonsykes <69767319+simonsykes@users.noreply.github.com>
2020-10-26 20:50:18 -0500

    Add files via upload

---
+++ ground.js
@@ -0,0 +1,14 @@
+class Ground{
+    constructor(){
+        var options={
+            isStatic: true
+        }
+        this.body=Bodies.rectangle(450,390,900,20,options);
+        World.add(world,this.body);
+    }
+    display(){
+        fill("red");
+        rectMode(CENTER);
+        rect(this.body.position.x,this.body.position.y,900,20);
+    }
+}
